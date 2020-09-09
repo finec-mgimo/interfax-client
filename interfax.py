3868b8d72d6ae792e0a3ed2e3cf5e4a9466a97ab
Evgeniy <e.pogrebnyak@gmail.com>
2020-09-09 09:34:16 +0300

    Update interfax.py

--- interfax.py
+++ interfax.py
@@ -1,20 +1,62 @@
 from contextlib import contextmanager
+from typing import Optional
 
-import xmlschema
-from zeep import Client
+import xmlschema  # type: ignore
+from zeep import Client  # type: ignore
 
-from settings import wdsl_url, login, password, get_schema_file 
+from settings import ENDPOINT, login, password, locate_schema_file
+
 
 @contextmanager
-def get_client(wdsl_url, login, password):
+def get_client(
+    login: Optional[str], password: Optional[str], wdsl_url: str = ENDPOINT
+) -> Client:
+    """Клиент для использования в конструкции with"""
+    # инициализируем и авторизуемся
     client = Client(wdsl_url)
     client.service.Authmethod(login, password)
     try:
+        # передаем в with
         yield client
     finally:
-        client.service.EndAll()
+        # закрываем сессию
+        client.service.End()
 
-with get_client(wdsl_url, login, password) as client:
+
+def get_schema(filename: str) -> xmlschema.XMLSchema10:
+    return xmlschema.XMLSchema(locate_schema_file(filename))
+
+
+def get_short_report(client, *arg, **kwarg) -> dict:
+    xml = client.service.GetCompanyShortReport(*arg, **kwarg).xmlData
+    return get_schema("ShortReport.xsd").to_dict(xml)["Data"]["Report"][0]
+
+
+# TODO:
+# - match schemas to methods
+# - generalise get_short_report()
+# - list available companies
+
+
+def get_schema_filename(method_name: str) -> Optional[str]:
+    return dict(GetCompanyShortReport="ShortReport.xsd").get(method_name)
+
+
+class Reporter:
+    def __init__(self, client):
+        self.client = client
+
+    def GetCompanyShortReport(self, *arg, **kwarg):
+        return get_short_report(self.client, *arg, **kwarg)
+
+
+with get_client(login, password) as client:
+    z = get_short_report(client, 210)
+    assert z["ShortNameEn"] == "Rosneft Oil Company"
+    w = Reporter(client).GetCompanyShortReport(210)
+    assert z == w
+
+with get_client(login, password) as client:
     # xml-ответ по схеме СПАРКа про компанию с таким Spark ID
     # (например, 210 для Роснефти или 158, кажется, для Газпрома)
     a = client.service.GetCompanyShortReport(210).xmlData
@@ -26,7 +68,7 @@ with get_client(wdsl_url, login, password) as client:
     b = client.service.GetCompanyShortReport(inn="7706107510").xmlData
 
     # с помощью схемы мы переводим XML в питоновский словарь, а его уже в JSON и в Mongo
-    Schema = xmlschema.XMLSchema(get_schema_file("ShortReport.xsd"))
+    Schema = xmlschema.XMLSchema(locate_schema_file("ShortReport.xsd"))
     data = Schema.to_dict(a)
 
 # Несколько проверок
@@ -59,4 +101,4 @@ assert [x for x in data["Data"]["Report"][0].keys()] == [
     "CompanyLiquidatedWithSameInfo",
 ]
 
-assert data['Data']['Report'][0]['INN'] == '7706107510'
+assert data["Data"]["Report"][0]["INN"] == "7706107510"
