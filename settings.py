1576827d20bb43fe72d6f4163a4292e44d8bce4d
Evgeniy <e.pogrebnyak@gmail.com>
2020-09-09 09:34:11 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ load_dotenv(dotenv_path=Path(".") / ".env")
 
 login = os.getenv("INTERFAX_LOGIN")
 password = os.getenv("INTERFAX_PASSWORD")
-wdsl_url = "http://sparkgatetest.interfax.ru/iFaxWebService/iFaxWebService.asmx?WSDL"
+ENDPOINT = "http://sparkgatetest.interfax.ru/iFaxWebService/iFaxWebService.asmx?WSDL"
 
 
-def get_schema_file(filename: str): 
+def locate_schema_file(filename: str):
     return str(Path(".") / "schemas" / filename)
